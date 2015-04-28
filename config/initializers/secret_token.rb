ee80b175a8da10a0eb079a33eb63299c52c6ba7a
Joshua B. Bussdieker <jbussdieker@gmail.com>
2015-04-27 22:36:46 -0700

    Rails new with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weblog::Application.config.secret_key_base = '75ddf3535e0b32e1ecd7efb39575d5036cd1ebc80138d4f410f5b79c76b900448bb2b88bde163ca84af7109e21ae3db03b448e94ffd5c2417f7773d4b784b326'
