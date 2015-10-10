cf13d36deb75aeb25e198f8b8b2541a7fe7da927
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2015-10-10 10:59:40 -0700

    Support markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def markdown(text)
+    html_renderer = Redcarpet::Render::HTML.new(render_options = {})
+    markdown = Redcarpet::Markdown.new(html_renderer, extensions = {})
+    markdown.render(text).html_safe
+  end
 end
