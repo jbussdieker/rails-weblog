baf5145abe472114f2945b086d805ae24324d95b
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2015-10-10 11:19:15 -0700

    Customize some options

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,16 @@
 module ApplicationHelper
   def markdown(text)
-    html_renderer = Redcarpet::Render::HTML.new(render_options = {})
-    markdown = Redcarpet::Markdown.new(html_renderer, extensions = {})
+    render_options = {
+      :hard_wrap => true,
+      :filter_html => true,
+    }
+    html_renderer = Redcarpet::Render::HTML.new(render_options)
+    extensions = {
+      :autolink => true,
+      :fenced_code_blocks => true,
+      :no_intra_emphasis => true,
+    }
+    markdown = Redcarpet::Markdown.new(html_renderer, extensions)
     markdown.render(text).html_safe
   end
 end
